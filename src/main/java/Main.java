b8073442db9a89484b709881c74bd290b120fb03
Shawn19702 <112501775+Shawn19702@users.noreply.github.com>
2022-10-04 13:57:33 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,38 @@
-public class Main {
+{
     public static void main(String[] args) {
         FlightCrewJob flightCrewJob1;
         FlightCrewJob flightCrewJob2;
         FlightCrewJob flightCrewJob3;
 
-        Crewmember crewmember = new Crewmember("Johnny", "CrewMember");
-        Crewmember crewmember1;
-        Crewmember crewmember2;
+        Crewmember crewmember = new Crewmember("Shawn", "Crewmember", FlightCrewJob.Pilot, 100000 );
+        Crewmember crewmember1 = new Crewmember("john", "Crewmember", FlightCrewJob.CO_Pilot, 60000 );
+        Crewmember crewmember2 = new Crewmember("bob", "Crewmember", FlightCrewJob.FLIGHT_Attendant, 40000 );
 
-        BusTicket busTicket;
-        PlaneTicket planeTicket;
+        BusTicket busTicket = new BusTicket(54, "USA", "California", "24a", 200 );
+        PlaneTicket planeTicket = new PlaneTicket(87, "USa", "Mexico", "24c", 250);
 
-        Passenger passenger57 = new Passenger("John", "Pasenger");
+Passenger passenger57 = new Passenger("SHawn", "Pasenger", busTicket, 23.0, new String[]{"2"});
 
-        crewmember.ServePerson(passenger57);
+        Passenger passenger07 = new Passenger("Bob", "Pasenger", planeTicket, 23.0, new String[]{"3"});
 
-        System.out.println(crewmember.ServePerson(passenger57));
+
+
+        passenger57.getAmOffLuggage();
+
+
+      //  passenger07.getAmOffLuggage(2);
+        crewmember1.ServePerson(passenger57);
+        crewmember1.ServePerson(passenger07);
+
+        passenger57.printTicket();
+
+        passenger07.printTicket();
+
+        busTicket.printPaymentAmount();
+
+        planeTicket.printPaymentAmount();
+
+
+
     }
 }
